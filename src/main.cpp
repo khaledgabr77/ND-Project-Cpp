8556aef34a186616f564eb1ca3b04eccbca30f35
khaled gabr <khaledgabr77@gmail.com>
2020-06-11 17:51:53 +0200

    Declare Start and end point using form user

--- src/main.cpp
+++ src/main.cpp
@@ -53,14 +53,24 @@ int main(int argc, const char **argv)
     }
     
     // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
+    float start_x, start_y, end_x, end_y;
     // user input for these values using std::cin. Pass the user input to the
+    std::cout << "Please Enter Your Coordinate : ";
+    std::cout << "Start X : ";
+    std::cin >> start_x;
+    std::cout << "Start Y : ";
+    std::cin >> start_y;
+    std::cout << "End X : ";
+    std::cin >> end_x;
+    std::cout << "End Y : ";
+    std::cin >> end_y;
     // RoutePlanner object below in place of 10, 10, 90, 90.
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
